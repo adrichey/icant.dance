c14bfcbf7db8a97edbc04c9fc18bb8c7417e1e05
Alan Richey <alan@icant.dance>
2018-04-28 18:10:05 -0500

    Initial commit. Intro card is done.

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <intro-card
+      headline="Alan Richey is a web developer."
+      sub-headline="He is also a huge geek that can't dance. Here are some statistics."
+    />
+  </div>
+</template>
+
+<script>
+import IntroCard from './components/IntroCard.vue';
+import MtgChart from './components/MtgChart.vue';
+
+export default {
+  name: 'app',
+  components: {
+    IntroCard,
+    MtgChart,
+  },
+}
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Alice|Playfair+Display');
+
+#app {
+  font-family: 'Alice', serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  font-size: 12px;
+}
+
+// Small devices (landscape phones, 576px and up)
+// @media (min-width: 576px) { ... }
+
+// Medium devices (tablets, 768px and up)
+// @media (min-width: 768px) { ... }
+
+// Large devices (desktops, 992px and up)
+// @media (min-width: 992px) { ... }
+
+// Extra large devices (large desktops, 1200px and up)
+// @media (min-width: 1200px) { ... }
+</style>
---
+++ src/components/IntroCard.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="intro-card">
+    <header>
+      <h1>{{ headline }}</h1>
+      <h2>{{ subHeadline }}</h2>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'intro-card',
+  props: {
+    headline: {
+      type: String,
+      required: true,
+    },
+    subHeadline: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.intro-card {
+  height: 100vh;
+  width: 100vw;
+  position: relative;
+  font-size: 12px;
+
+  header {
+    position: absolute;
+    top: 50%;
+    margin: -6em 5% 0 5%;
+    width: 90%;
+
+    h1 {
+      margin: 0;
+      font-family: 'Playfair Display', serif;
+      font-size: 3em;
+      margin-bottom: .2em;
+    }
+
+    h2 {
+      margin: 0;
+      font-size: 1.2em;
+    }
+  }
+}
+
+@media (min-width: 992px) {
+  .intro-card header {
+    h1 {
+      font-size: 5em;
+    }
+
+    h2 {
+      font-size: 2em;
+    }
+  }
+}
+</style>
